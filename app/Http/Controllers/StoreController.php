41641078f3c3e072b9ef15a98327f7d004ab06ec
Lajtaib0801 <lajtaibenja@gmail.com>
2024-04-19 18:41:58 +0200

    in storeController index and show added

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -10,9 +10,18 @@ class StoreController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Store::all();
+        $stores = Store::paginate($request['limit'] ?? 10);
+        if ($request['withCity'] == 'true') {
+            $stores = $stores->load('city');
+        }
+        if (is_null($stores)) {
+            return response()->json([
+               'message' => 'No store found'
+            ], 404);
+        }
+        return $stores;
     }
 
     /**
@@ -26,9 +35,18 @@ class StoreController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Store $store)
+    public function show(Request $request, $id)
     {
-        //
+        $store = Store::find($id);
+        if ($request['withCity'] == 'true') {
+            $store = $store->load('city');
+        }
+        if (is_null($store)) {
+            return response()->json([
+               'message' => 'Store not found'
+            ], 404);
+        }
+        return $store;
     }
 
     /**
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Store extends Model
 {
@@ -12,5 +13,13 @@ class Store extends Model
     public $timestamps = false;
     protected $guarded = [];
 
-    
+    /**
+     * Get the city that owns the Store
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'city', 'id');
+    }
 }
