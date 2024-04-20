9baf9f3a324a4a85bd061406540f3b3741d480e4
Lajtaib0801 <lajtaibenja@gmail.com>
2024-04-20 14:20:45 +0200

    StoreController query params added

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\Models\Store;
 use Illuminate\Http\Request;
 
@@ -12,13 +13,38 @@ class StoreController extends Controller
      */
     public function index(Request $request)
     {
-        $stores = Store::paginate($request['limit'] ?? 10);
-        if ($request['withCity'] == 'true') {
-            $stores = $stores->load('city');
+        $query = Store::query();
+        if (!is_null($request['name'])) {
+            $query->where('name', 'like', '%' . $request['name'] . '%');
         }
+        if (!is_null($request['city'])) {
+            $cityId = City::where('name', $request['city'])->first()?->id;
+            if (is_null($cityId)) {
+                return response()->json([
+                    'message' => 'City not found'
+                ], 404);
+            }
+            $query->where('city', $cityId);
+        }
+        if (!is_null($request['address'])) {
+            $query->where('address', 'like', '%' . $request['address'] . '%');
+        }
+        if (!is_null($request['foundedDate'])) {
+            $query->where('foundedDate', $request['foundedDate']);
+        }
+        if (!is_null($request['openingTime'])) {
+            $query->where('openingTime', $request['openingTime']);
+        }
+        if (!is_null($request['closingTime'])) {
+            $query->where('closingTime', $request['closingTime']);
+        }
+        if ($request['withCity'] == 'true') {
+            $query->with('city');
+        }
+        $stores = $query->paginate($request['limit'] ?? 10);
         if ($stores->isEmpty()) {
             return response()->json([
-               'message' => 'No store found'
+                'message' => 'No store found'
             ], 404);
         }
         return $stores;
@@ -43,7 +69,7 @@ class StoreController extends Controller
         }
         if (is_null($store)) {
             return response()->json([
-               'message' => 'Store not found'
+                'message' => 'Store not found'
             ], 404);
         }
         return $store;
